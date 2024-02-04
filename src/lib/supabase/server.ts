bd66ebf5d3d1a29024a13cf14bda1efd72276189
Dillpickleschmidt <drkuzmick@gmail.com>
2024-02-03 20:11:48 -0500

    Auth fix

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -2,24 +2,31 @@
 import { createServerClient, type CookieOptions } from "@supabase/ssr"
 import { cookies } from "next/headers"
 
-const supabase = createServerClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  {
-    cookies: {
-      get(name: string) {
-        return cookies().get(name)?.value
+export async function getSupabase(serverComponent = false) {
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name) {
+          //return cookie with the name 'name' here
+          return cookies().get(name)?.value
+        },
+        set(name, value, options) {
+          // set the cookie
+          if (serverComponent) return
+          cookies().set(name, value, options)
+        },
+        remove(name, options) {
+          // delete the cookie
+          if (serverComponent) return
+          cookies().set(name, "", options)
+        },
       },
-      set(name: string, value: string, options: CookieOptions) {
-        cookies().set({ name, value, ...options })
-      },
-      remove(name: string, options: CookieOptions) {
-        cookies().set({ name, value: "", ...options })
-      },
-    },
-  }
-)
+    }
+  )
+}
 
-export default async function getSupabase() {
-  return supabase
+export default async function createSupabaseServerComponentClient() {
+  return getSupabase(true)
 }
