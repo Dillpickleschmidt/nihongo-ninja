eda3c3b85cc746f2d1232daa2ec9a77ae6ae9905
Dillpickleschmidt <drkuzmick@gmail.com>
2024-02-03 22:25:45 -0500

    Auth fixes

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -3,8 +3,12 @@ import { NextResponse } from "next/server"
 import { type CookieOptions, createServerClient } from "@supabase/ssr"
 
 export async function GET(request: Request) {
-  const { searchParams, origin } = new URL(request.url)
+  const requestUrl = new URL(request.url)
+
+  const { searchParams, origin } = requestUrl
   const code = searchParams.get("code")
+  // if "next" is in param, use it as the redirect URL
+  const next = searchParams.get("next") ?? "/"
 
   if (code) {
     const cookieStore = cookies()
@@ -27,7 +31,7 @@ export async function GET(request: Request) {
     )
     const { error } = await supabase.auth.exchangeCodeForSession(code)
     if (!error) {
-      return NextResponse.redirect(`${origin}/learn`)
+      return NextResponse.redirect(requestUrl.origin + "/learn")
     }
   }
 
