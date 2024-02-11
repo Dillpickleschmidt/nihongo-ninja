9c5157cd9d58a9b87323b7553869bfc759c3a08f
Dillpickleschmidt <drkuzmick@gmail.com>
2024-02-11 14:19:31 -0500

    Added practice pages for all L7 vocab

---
+++ src/app/learn/chapter-7/vocab-learn-all/page.tsx
@@ -0,0 +1,21 @@
+import LearnVocab from "@/components/learn-vocab/LearnVocab"
+import vocabData from "@/app/learn/chapter-7/lesson-summary/data.json"
+import Dialog from "@/components/Dialog"
+
+export default function page() {
+  return (
+    <Dialog variant={"large"} className="border-4 border-black bg-[#191919]">
+      <div className="text-white overscroll-y-contain">
+        <LearnVocab
+          data={vocabData}
+          link="/learn/chapter-7/lesson-7"
+          shuffleTerms={true}
+        >
+          <h1 className="text-7xl font-medium text-[#F8F5E9]">
+            Now practice the kanji!
+          </h1>
+        </LearnVocab>
+      </div>
+    </Dialog>
+  )
+}
