79af5fd8b5c91a756a3f835deff150b5ca9fb1c7
Dillpickleschmidt <drkuzmick@gmail.com>
2024-02-19 22:17:31 -0500

    Chapter 9 Personal Lessons

---
+++ src/app/learn/chapter-9/vocab-learn-lesson-4/page.tsx
@@ -0,0 +1,24 @@
+import LearnVocab from "@/components/learn-vocab/LearnVocab"
+import vocabData from "@/app/learn/chapter-9/lesson-4/data.json"
+import Dialog from "@/components/Dialog"
+import ConvertToEnglishKey from "@/components/learn-vocab/convertToEnglishKey"
+
+const convertedData = ConvertToEnglishKey({ data: vocabData })
+
+export default function page() {
+  return (
+    <Dialog variant={"large"} className="border-4 border-black bg-[#191919]">
+      <div className="text-white overscroll-y-contain">
+        <LearnVocab
+          data={convertedData}
+          link="/learn/chapter-9/vocab-learn-lesson-4-kanji"
+          shuffleTerms={true}
+        >
+          <h1 className="text-7xl font-medium text-[#F8F5E9]">
+            Practice the hiragana first!
+          </h1>
+        </LearnVocab>
+      </div>
+    </Dialog>
+  )
+}
