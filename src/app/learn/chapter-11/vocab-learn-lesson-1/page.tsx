dfd4a54a10eb7d3aa666155cc1e6358185bf4bdb
Dillpickleschmidt <drkuzmick@gmail.com>
2024-03-28 09:52:08 -0400

    Some more personal vocab practice

---
+++ src/app/learn/chapter-11/vocab-learn-lesson-1/page.tsx
@@ -0,0 +1,24 @@
+import LearnVocab from "@/components/learn-vocab/LearnVocab"
+import vocabData from "@/app/learn/chapter-11/lesson-1/data.json"
+import Dialog from "@/components/Dialog"
+import ConvertToEnglishKey from "@/components/learn-vocab/convertToEnglishKey"
+
+const convertedData = ConvertToEnglishKey({ data: vocabData })
+
+export default function page() {
+  return (
+    <Dialog variant={"large"} className="border-4 border-black bg-[#191919]">
+      <div className="text-white overscroll-y-contain">
+        <LearnVocab
+          data={convertedData}
+          link="/learn/chapter-11/lesson-2"
+          shuffleTerms={true}
+        >
+          <h1 className="text-7xl font-medium text-[#F8F5E9]">
+            Now practice the hiragana!
+          </h1>
+        </LearnVocab>
+      </div>
+    </Dialog>
+  )
+}
