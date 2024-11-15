4acc386f5bfb7aaaeab075fe71de5634b263e842
Dillpickleschmidt <drkuzmick@gmail.com>
2024-11-15 17:17:32 -0500

    style: fixed navbar on /learn page

--- src/features/navbar/Nav.tsx
+++ src/features/navbar/Nav.tsx
@@ -18,81 +18,88 @@ export default function Nav() {
       : "border-transparent hover:border-orange-200 saturate-[50%]"
 
   return (
-    <nav class="relative z-50 bg-background">
-      <ul class="container flex items-center justify-between px-2 py-3 dark:text-orange-200 sm:px-3">
-        <NavigationMenu>
-          <NavigationMenuItem>
-            <A href="/">
-              <NavigationMenuTrigger
-                class="px-2 text-base font-normal sm:px-4"
-                withArrow={false}
-              >
-                <span class={`border-b-2 ${active("/")} saturate-[50%]`}>
-                  Home
-                </span>
-              </NavigationMenuTrigger>
-            </A>
-            <NavigationMenuContent>
-              <div class="flex h-48 w-64 items-center justify-center">
-                Home Preview
-              </div>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
+    <>
+      <nav
+        class={`${location.pathname === "/learn" ? "fixed w-full" : "relative"} z-50 bg-background`}
+      >
+        <ul class="container flex items-center justify-between px-2 py-3 dark:text-orange-200 sm:px-3">
+          <NavigationMenu>
+            <NavigationMenuItem>
+              <A href="/">
+                <NavigationMenuTrigger
+                  class="px-2 text-base font-normal sm:px-4"
+                  withArrow={false}
+                >
+                  <span class={`border-b-2 ${active("/")} saturate-[50%]`}>
+                    Home
+                  </span>
+                </NavigationMenuTrigger>
+              </A>
+              <NavigationMenuContent>
+                <div class="flex h-48 w-64 items-center justify-center">
+                  Home Preview
+                </div>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
 
-          <NavigationMenuItem>
-            <A href="/learn">
-              <NavigationMenuTrigger class="pl-2 pr-1 text-base font-normal sm:pl-3 sm:pr-2">
-                <span class={`border-b-2 ${active("/learn")} saturate-[50%]`}>
-                  Learn
-                </span>
-              </NavigationMenuTrigger>
-            </A>
-            <NavigationMenuContent>
-              <NavigationMenuLink class="flex h-96 w-72 items-center justify-center">
-                <div>Learn Options</div>
-              </NavigationMenuLink>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
+            <NavigationMenuItem>
+              <A href="/learn">
+                <NavigationMenuTrigger class="pl-2 pr-1 text-base font-normal sm:pl-3 sm:pr-2">
+                  <span class={`border-b-2 ${active("/learn")} saturate-[50%]`}>
+                    Learn
+                  </span>
+                </NavigationMenuTrigger>
+              </A>
+              <NavigationMenuContent>
+                <NavigationMenuLink class="flex h-96 w-72 items-center justify-center">
+                  <div>Learn Options</div>
+                </NavigationMenuLink>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
 
-          <NavigationMenuTrigger
-            as="a"
-            href="/about"
-            class="px-2 text-base font-normal sm:px-4"
-            withArrow={false}
-          >
-            <span class={`border-b-2 ${active("/about")} saturate-[50%]`}>
-              About
-            </span>
-          </NavigationMenuTrigger>
-        </NavigationMenu>
-        <li>
-          <Button
-            as="a"
-            href="/learn/sentence-practice"
-            class="px-2 text-base font-normal sm:px-4"
-            variant="ghost"
-          >
-            <span class={`border-b-2 ${active("/about")} saturate-[50%]`}>
-              文
-            </span>
-            <Pencil size="16px" class="text-yellow-400 lg:ml-3" />
-          </Button>
-          <Button
-            as="a"
-            href="/learn/conjugation"
-            class="px-2 text-base font-normal sm:px-4"
-            variant="ghost"
-          >
-            <span class={`border-b-2 ${active("/about")} saturate-[50%]`}>
-              活用
-            </span>
-            <GraduationCap size="20px" class="text-teal-400 lg:ml-3" />
-          </Button>
-          <div class="inline-flex saturate-[50%]">
-            <ModeToggle />
-          </div>
-        </li>
-      </ul>
-    </nav>
+            <NavigationMenuTrigger
+              as="a"
+              href="/about"
+              class="px-2 text-base font-normal sm:px-4"
+              withArrow={false}
+            >
+              <span class={`border-b-2 ${active("/about")} saturate-[50%]`}>
+                About
+              </span>
+            </NavigationMenuTrigger>
+          </NavigationMenu>
+          <li>
+            <Button
+              as="a"
+              href="/learn/sentence-practice"
+              class="px-2 text-base font-normal sm:px-4"
+              variant="ghost"
+            >
+              <span class={`border-b-2 ${active("/about")} saturate-[50%]`}>
+                文
+              </span>
+              <Pencil size="16px" class="text-yellow-400 lg:ml-3" />
+            </Button>
+            <Button
+              as="a"
+              href="/learn/conjugation"
+              class="px-2 text-base font-normal sm:px-4"
+              variant="ghost"
+            >
+              <span class={`border-b-2 ${active("/about")} saturate-[50%]`}>
+                活用
+              </span>
+              <GraduationCap size="20px" class="text-teal-400 lg:ml-3" />
+            </Button>
+            <div class="inline-flex saturate-[50%]">
+              <ModeToggle />
+            </div>
+          </li>
+        </ul>
+      </nav>
+      <div
+        class={`${location.pathname === "/learn" ? "block h-16" : "hidden"}`}
+      />
+    </>
   )
 }
