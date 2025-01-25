bb2ff18e90a3ab657e58fa25a9eef2014a69c5a8
Dillpickleschmidt <drkuzmick@gmail.com>
2025-01-25 17:25:52 -0500

    style: add "lg" ContentBox size

--- src/components/ContentBox.tsx
+++ src/components/ContentBox.tsx
@@ -1,17 +1,33 @@
 import { Show } from "solid-js"
 import { Button } from "./ui/button"
 import { A } from "@solidjs/router"
+import { cva } from "class-variance-authority"
+import { cn } from "@/libs/cn"
 
 type ContentBoxProps = {
   nextButtonText?: string
   nextButtonLink?: string
   children: any
+  size?: "default" | "lg"
+  class?: string
 }
+
+export const contentBoxVariants = cva("relative w-full bg-background/75", {
+  variants: {
+    size: {
+      default: "max-w-[825px] ",
+      lg: "max-w-5xl",
+    },
+  },
+  defaultVariants: {
+    size: "default",
+  },
+})
 
 export default function ContentBox(props: ContentBoxProps) {
   return (
     <div class="flex w-full justify-center">
-      <div class="relative w-full max-w-[825px] bg-background/75">
+      <div class={cn(contentBoxVariants({ size: props.size }), props.class)}>
         {props.children}
         <Show when={props.nextButtonText && props.nextButtonLink}>
           <div class="absolute bottom-16 right-16">
