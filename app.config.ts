08d611d9611c05aff953486cc4f902eafda64466
Dillpickleschmidt <drkuzmick@gmail.com>
2025-02-08 22:04:15 -0500

    update config for amplify

--- app.config.ts
+++ app.config.ts
@@ -1,23 +1,43 @@
 import { defineConfig } from "@solidjs/start/config"
 import { dirname, resolve } from "node:path"
-import { fileURLToPath, URL } from "node:url"
+import { fileURLToPath } from "node:url"
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 export default defineConfig({
+  server: {
+    preset: "aws-amplify",
+  },
   vite: {
     resolve: {
       alias: {
         "@": resolve(__dirname, "./src"),
-        // To fix loading ALL icons in dev mode w/Vite
-        // "lucide-solid/icons": fileURLToPath(
-        //   new URL(
-        //     "./node_modules/lucide-solid/dist/source/icons",
-        //     import.meta.url,
-        //   ),
-        // ),
       },
+    },
+    define: {
+      // Client-side variables
+      "process.env.VITE_POSTHOG_KEY": JSON.stringify(
+        process.env.VITE_POSTHOG_KEY,
+      ),
+      "process.env.VITE_POSTHOG_HOST": JSON.stringify(
+        process.env.VITE_POSTHOG_HOST,
+      ),
+      "process.env.VITE_SUPABASE_URL": JSON.stringify(
+        process.env.VITE_SUPABASE_URL,
+      ),
+      "process.env.VITE_SUPABASE_ANON_KEY": JSON.stringify(
+        process.env.VITE_SUPABASE_ANON_KEY,
+      ),
+      "process.env.VITE_GOOGLE_CLIENT_ID": JSON.stringify(
+        process.env.VITE_GOOGLE_CLIENT_ID,
+      ),
+
+      // Server-side variables
+      "process.env.SUPABASE_URL": JSON.stringify(process.env.SUPABASE_URL),
+      "process.env.SUPABASE_ANON_KEY": JSON.stringify(
+        process.env.SUPABASE_ANON_KEY,
+      ),
     },
   },
 })
