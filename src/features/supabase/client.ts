eff0109b40efd3de92083b58037798c7966e0200
Dillpickleschmidt <drkuzmick@gmail.com>
2025-02-08 20:48:19 -0500

    fix: use server supabase env var

--- src/features/supabase/client.ts
+++ src/features/supabase/client.ts
@@ -16,28 +16,31 @@ export function createFrontendClient() {
 
 export function createBackendClient() {
   "use server"
-  return createServerClient(
-    process.env.VITE_SUPABASE_URL!,
-    process.env.VITE_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        getAll() {
-          return parseCookieHeader(
-            getRequestEvent()?.request.headers.get("Cookie") ?? "",
-          )
-        },
-        setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value, options }) =>
-            getRequestEvent()?.response.headers.append(
-              "Set-Cookie",
-              serializeCookieHeader(name, value, options),
-            ),
-          )
-        },
+  const supabaseUrl = process.env.SUPABASE_URL
+  const supabaseAnonKey = process.env.SUPABASE_ANON_KEY
+
+  if (!supabaseUrl || !supabaseAnonKey) {
+    throw new Error("Missing Supabase environment variables")
+  }
+
+  return createServerClient(supabaseUrl, supabaseAnonKey, {
+    cookies: {
+      getAll() {
+        return parseCookieHeader(
+          getRequestEvent()?.request.headers.get("Cookie") ?? "",
+        )
       },
-      auth: {
-        autoRefreshToken: false,
+      setAll(cookiesToSet) {
+        cookiesToSet.forEach(({ name, value, options }) =>
+          getRequestEvent()?.response.headers.append(
+            "Set-Cookie",
+            serializeCookieHeader(name, value, options),
+          ),
+        )
       },
     },
-  )
+    auth: {
+      autoRefreshToken: false,
+    },
+  })
 }
