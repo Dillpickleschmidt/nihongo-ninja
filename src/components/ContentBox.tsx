f42e228745015a30774587eae84d96ff8cf731ac
Dillpickleschmidt <drkuzmick@gmail.com>
2025-02-09 13:00:11 -0500

    selectively update ContentBox, UnitButton, & jap-pron lesson from last commit

--- src/components/ContentBox.tsx
+++ src/components/ContentBox.tsx
@@ -29,7 +29,11 @@ export const contentBoxVariants = cva("relative w-full bg-background/75", {
 export default function ContentBox(props: ContentBoxProps) {
   const navigate = useNavigate()
   const supabase = createFrontendClient()
-  const { user } = useLearnPageContext()
+  const {
+    user,
+    completedModules,
+    // setCompletedModules
+  } = useLearnPageContext()
 
   async function handleClick(e: Event, nextButtonLink: string) {
     e.preventDefault()
@@ -53,6 +57,12 @@ export default function ContentBox(props: ContentBoxProps) {
         console.error("Error recording module completion:", err)
       }
     }
+    // setCompletedModules({
+    //   data: [
+    //     ...(completedModules()?.data ?? []),
+    //     { module_path: window.location.pathname.replace(/^\/learn\//, "") },
+    //   ],
+    // })
 
     navigate(nextButtonLink)
   }
@@ -70,7 +80,7 @@ export default function ContentBox(props: ContentBoxProps) {
               class="border-green-500/35 bg-green-500/15 hover:bg-green-500/25"
               onClick={(e) => handleClick(e, "/learn")}
             >
-              {props.nextButtonText ?? "<- Complete & Return"}
+              {"<-"} Complete & Return
             </Button>
             <Button
               as="a" // so users see the link preview when hovering
--- src/features/learn-page/chapter-box/UnitButton.tsx
+++ src/features/learn-page/chapter-box/UnitButton.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button"
 import UnitButtonContents from "./UnitButtonContents"
 import { UnitButtonType } from "./types"
 import { useLearnPageContext } from "../context/LearnPageContext"
+import { createMemo } from "solid-js"
 
 type UnitButtonProps = {
   children: string
@@ -16,11 +17,11 @@ type UnitButtonProps = {
 export default function UnitButton(props: UnitButtonProps) {
   const { completedModules } = useLearnPageContext()
   const strippedLink = props.link?.replace(/^\/learn\//, "")
-  const isCompleted = completedModules()?.data?.find(
-    (e) => e.module_path === strippedLink,
-  )
-    ? true
-    : false
+  const isCompleted = createMemo(() => {
+    return completedModules().data?.find((e) => e.module_path === strippedLink)
+      ? true
+      : false
+  })
 
   return (
     <div class="!p-0 duration-75 ease-in-out hover:scale-[98.5%]">
@@ -29,16 +30,16 @@ export default function UnitButton(props: UnitButtonProps) {
         as={props.disabled ? undefined : "a"}
         href={props.disabled ? undefined : props.link}
         variant="outline"
-        class={`relative h-12 w-full whitespace-nowrap text-sm font-normal ${isCompleted && "border-green-500/50 font-bold text-green-500"}`}
+        class={`relative h-12 w-full whitespace-nowrap text-sm font-normal ${isCompleted() && "border-green-500/50 font-bold text-green-500"}`}
         disabled={props.disabled}
       >
         <div
-          class={`absolute inset-0 flex items-center justify-between overflow-y-hidden overflow-x-scroll px-6 no-scrollbar ${isCompleted && "bg-green-500/10"}`}
+          class={`absolute inset-0 flex items-center justify-between overflow-y-hidden overflow-x-scroll px-6 no-scrollbar ${isCompleted() && "bg-green-500/10"}`}
         >
           <UnitButtonContents
             id={props.number}
             types={props.types}
-            isCompleted={isCompleted}
+            isCompleted={isCompleted()}
           >
             {props.children}
           </UnitButtonContents>
